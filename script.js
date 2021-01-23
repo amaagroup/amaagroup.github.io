3a72e91929d2edb285c8cb56e080dfbdf8aa3a2f
amaagroup <76109137+amaagroup@users.noreply.github.com>
2021-01-23 16:14:15 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function noti{
-	alert('welcome to our website');
+var titl=true;
+var sta=true;
+function bmenu() {
+	if (sta) document.getElementById('sub-bmenu').style.display = "block";
+	else document.getElementById('sub-bmenu').style.display = "none";
+	sta=1-sta;
+}
+function f1() {
+  if (titl)	document.getElementById('tit').innerHTML = 'Home';
+  else document.getElementById('tit').innerHTML = 'Trang chủ';
+  titl = 1-titl;
+
 }
