49742c18673ef43a8c801f6c9eed343f893f9199
amaagroup <76109137+amaagroup@users.noreply.github.com>
2021-02-04 19:47:52 +0700

    update table

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ function bmenu() {
 	sta=1-sta;
 	
 }
-function f1() {
+/*function f1() {
   if (titl)	document.getElementById('tit').innerHTML = 'Home';
   else document.getElementById('tit').innerHTML = 'Trang chủ';
   titl = 1-titl;
 
 }
+function zo(){
+	document.getElementById('zo').className ='classname';
+	document.getElementById('zo').style =" font-family: 'Montserrat', sans-serif;font-weight:bold; color:white; margin-top:300px;cursor:default;";
+}*/
