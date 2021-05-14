b64244e5b51b1073480e5f7d9a806574d2b109b6
amaagroup <76109137+amaagroup@users.noreply.github.com>
2021-05-14 09:10:25 +0700

    volume bar

--- MusicSTimer/app.js
+++ MusicSTimer/app.js
@@ -9,65 +9,6 @@ var dots = document.getElementsByClassName("transiDot");
 var lists = document.getElementsByClassName("musicList");
 
 
-function timer(){
-    var music = document.getElementsByTagName('audio')[0] ;
-    var time=document.getElementById('timer');
-    var total = Math.floor(music.duration);
-    var now = Math.floor(music.currentTime);
-    var dot= document.getElementById('d');
-    var a=63-(now/total)*115;
-
-    var mm,ss,mmm,sss;
-    mm=Math.floor(now/60);
-    ss=now - 60*mm;
-    mmm=Math.floor(total/60);
-    sss=total - 60*mmm;
-    dot.style.left = 'calc(50% - ' + a + 'px)';
-    time.innerHTML = mm + ':' + ss + '/' + mmm + ':' + sss;
-    
-}
-function mute()
-{
-    var music = document.getElementsByTagName('audio')[0] ;
-    var mute=document.getElementById('mute');
-    if (!music.muted) {music.muted = true;mute.style.backgroundColor= 'red';}
-    else {music.muted=false; mute.style.backgroundColor= '#8060c6';}
-
-}
-function po_lay(){
-    var music = document.getElementsByTagName('audio')[0] ;
-    var but=document.getElementsByClassName('paupay');
-   if (p) {music.play();p=false;but[0].style.display = 'none';but[1].style.display = 'block';}
-   else {music.pause();p=true;but[1].style.display = 'none';but[0].style.display = 'block';}
-    // music.loop = true;
-    setInterval(timer,1000);
-
-}
-function ChonBai(n){
-    var song=document.getElementById('songWillPlay');
-    switch (n){
-    case 1: song.setAttribute("src","./Resources/COOLIN_DUB'L U.mp3");break;
-    case 2: song.setAttribute("src","./Resources/YeuSacYeu.mp3");break;
-    }
-    document.getElementsByTagName('audio')[0].load();
-    p=true;
-    po_lay();
-}
-
-function Tua(x)
-{
-    var music = document.getElementsByTagName('audio')[0];
-    var start = innerWidth*0.5 - 60;
-    var progess = Math.floor((x - start)/125  * music.duration);
-    music.currentTime = progess;
-    timer();
-
-}
-/////////////////////////////////////////////////////////////////////////////////////////////////// 
-// 
-// 
-// 
-///////////////////////////////////////////////////////////////////////////////////////////////////
 function tapPlay() {
 
 	var btn = document.getElementById('playButton');
