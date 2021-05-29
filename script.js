73cc16b79bfbceff12eb84d42a115a8f5e086eb5
amaagroup <76109137+amaagroup@users.noreply.github.com>
2021-05-29 19:47:52 +0700

    update css

--- script.js
+++ script.js
@@ -3,16 +3,5 @@ var sta=true;
 function bmenu() {
 	if (sta) document.getElementById('sub-bmenu').style.display = "block";
 	else document.getElementById('sub-bmenu').style.display = "none";
-	sta=1-sta;
-	
+	sta=1-sta;	
 }
-/*function f1() {
-  if (titl)	document.getElementById('tit').innerHTML = 'Home';
-  else document.getElementById('tit').innerHTML = 'Trang chủ';
-  titl = 1-titl;
-
-}
-function zo(){
-	document.getElementById('zo').className ='classname';
-	document.getElementById('zo').style =" font-family: 'Montserrat', sans-serif;font-weight:bold; color:white; margin-top:300px;cursor:default;";
-}*/
