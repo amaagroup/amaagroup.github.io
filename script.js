2187a4c17f2f3c86c214ddcc6870a78842d96b04
amaagroup <amaagroup@github.com>
2021-07-14 15:37:30 +0700

    update new sidebar

--- script.js
+++
@@ -1,7 +0,0 @@
-var titl=true;
-var sta=true;
-function bmenu() {
-	if (sta) document.getElementById('sub-bmenu').style.display = "block";
-	else document.getElementById('sub-bmenu').style.display = "none";
-	sta=1-sta;	
-}
